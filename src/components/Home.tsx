445a32a4b93e2e2d165f653feee08a7483b667b9
Sulaiman Albulaihy <33493055+Sulaiman122@users.noreply.github.com>
2022-03-03 08:20:07 +0300

    Update Home.tsx

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -2,6 +2,8 @@ import Discover from "./Discover";
 import Table from "./Table";
 import Form from "./Form";
 import { useEffect, useState } from "react";
+import { Button } from "antd";
+import "antd/dist/antd.css";
 
 function Home() {
   const [show, setShow] = useState(false);
@@ -18,6 +20,10 @@ function Home() {
         <button id="showText" onClick={() => show?setShow(false):setShow(true)}>
           {show?<p>Hide text</p>:<p>Show text</p>}
         </button>
+        //with ant design
+        <Button type="primary" onClick={() => (show ? setShow(false) : setShow(true))}>
+          {show ? <p>Hide text</p> : <p>Show text</p>}
+        </Button>
         {show ? <p>I'm hidden Text ..</p> : <></>}
       </div>
       <Table />
